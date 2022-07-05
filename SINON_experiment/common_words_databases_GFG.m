34661e3e2752738150fa93f5e876dadbd4363c4c
gorkafraga <gorkafraga@gmail.com>
2022-07-05 11:48:02 +0200

    unespecific changes

--- SINON_experiment/common_words_databases_GFG.m
+++ SINON_experiment/common_words_databases_GFG.m
@@ -1,14 +1,17 @@
 clear all
 
 %% Loading data
- multipic = readtable('C:\Users\gfraga\Downloads\Neuroling_SINON_stimuli', 'Sheet',1);
- subtlex = readtable('C:\Users\gfraga\Downloads\Neuroling_SINON_stimuli', 'Sheet',2);
- clearpond = readtable('C:\Users\gfraga\Downloads\Neuroling_SINON_stimuli', 'Sheet',3);
- wuggy = readtable('C:\Users\gfraga\Downloads\Neuroling_SINON_stimuli', 'Sheet',4);
-
+ multipic = readtable('Y:\Neuroling_SINON_stimuli.xlsx', 'Sheet','Multipic');
+ subtlex = readtable('Y:\Neuroling_SINON_stimuli.xlsx', 'Sheet','Subtlex');
+ clearpond = readtable('Y:\Neuroling_SINON_stimuli.xlsx', 'Sheet','Clearpond');
+ wuggy = readtable('Y:\Neuroling_SINON_stimuli.xlsx', 'Sheet','Wuggy');
+ wuggy_selected_alt = readtable('Y:\Neuroling_SINON_stimuli.xlsx', 'Sheet','Wuggy_selected_alt');
+ 
+ 
 %% Merge datasets
 tbl1 = join(multipic,subtlex,'keys','ITEM'); % assuming the variable ITEM is common to all sets
 merged_database = join(tbl1,clearpond,'keys','ITEM');
+
 
 %% Remove duplicate names 
 merged_database.Word = merged_database.Word_clearpond;
@@ -32,9 +35,11 @@ end
 wuggy_selected = wuggy(indices,:); 
 
 %% Add to merged set without duplicates 
+tbl2merge = merged_clean(ismember(merged_clean.Word,wuggy_selected_alt.Word),:);
+ismember(merged_clean.Word,wuggy_selected_alt.Word)
+allmerged = join(tbl2merge,wuggy_selected_alt,'keys','Word');
 
-merged_clean = outerjoin(merged_clean,wuggy_selected,'Type','Left','MergeKeys',true);
-
+writetable(allmerged,"C:\Users\gfraga\scripts_neulin\SINON_experiment\all_merged.xls")
 %% 
 %writetable(wuggy_selected,"C:\Users\gfraga\scripts_neulin\SINON_experiment\Wuggy_selected.xls")
-%writetable(merged_clean,"C:\Users\gfraga\scripts_neulin\SINON_experiment\Merged_clean.xls")
+
