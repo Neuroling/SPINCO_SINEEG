840ac73043e7d63ea7f1c30968e8462c83bc6a13
gorkafraga <gorkafraga@gmail.com>
2022-07-12 11:58:51 +0200

    revised comments, cleaned, adapted call for vocoder

--- Noise_generation/Run_speechShapeNoise.m
+++ Noise_generation/Run_speechShapeNoise.m
@@ -46,12 +46,12 @@ noctaves =      6;   % 1/6 octave band smoothing (spectral smoothing in which th
 target_dB_snr = -10:5:10;
 
 %% Read normalize, filter and concatenate 
-disp('...normalizing speech (rms) and filtering')
+disp('>> Normalizing speech (meadian rms of all files) and filtering')
 
 % Filter
 signals = cellfun(@(x) audioread(x), wavfiles, 'UniformOutput',0);
 NyqFreq = srate/2;            % fs = 2kHz; Nyquist freq. = 1 kHz
-[filt_b,filt_a]=butter(3, [filt_low filt_upper]/NyqFreq);
+[filt_b,filt_a]=butter(3, [filt_low filt_upper]/NyqFreq); % make butterworth filter
 sigs_filt = cellfun(@(x) filtfilt(filt_b,filt_a,x), signals,'UniformOutput',0);
 
 
@@ -72,14 +72,13 @@ sigs_filt_norm = cellfun(@(x) x.*RMS_median/rms(x), sigs_filt, 'UniformOutput', 
  
  %% Loop thru SNR values 
   for L=1:length(target_dB_snr)
-     disp(['Start SNR loop']);
      
-     dblevel = target_dB_snr(L); 
-     target_lin_snr = db2mag(dblevel);  % db2mag matlab func does: 10.^(target_dB_SNR/20);
-     
-     %% Add noise to each file  with intro and outro noise and ramps
-     disp('...introducing startup noise + ramp in in SiSSN words')
-     
+    disp(['....Start SNR loop']);       
+    %%% Add noise to each file  with intro and outro noise and ramps
+    dblevel = target_dB_snr(L); 
+    target_lin_snr = db2mag(dblevel);  % db2mag matlab func does: 10.^(target_dB_SNR/20);
+    
+    
      sigs_filt_norm_adj = cell(length(sigs_filt_norm),1);
      SiSSN= cell(length(sigs_filt_norm),1);
      for i = 1:length(sigs_filt_norm)
@@ -92,15 +91,15 @@ sigs_filt_norm = cellfun(@(x) x.*RMS_median/rms(x), sigs_filt, 'UniformOutput', 
          rampout = [flip(linspace(0,1,rampout_length*srate)),zeros(1,tail_points-rampout_length*srate)];
          
          if head_points + tail_points + length(sigs_filt_norm_adj{i}) > length(ssn)
-             error('You try to add too long head/tail noise periods');
+             error('You tried to add too long head/tail noise periods');
          end
          
          % normalize noise
-         disp('normalizing SSN to the individual signal rms');
+         disp('normalizing SSN to rms of current signal');
          ssn_norm = ssn.*(rms(sigs_filt_norm{i})/rms(ssn));
          
        
-      %%%% noise to signal
+      %%%% add noise to signal
          % adjust signal intensity
          sigs_filt_norm_adj{i} = sigs_filt_norm{i}.*target_lin_snr;
          
@@ -110,60 +109,62 @@ sigs_filt_norm = cellfun(@(x) x.*RMS_median/rms(x), sigs_filt, 'UniformOutput', 
          noise2use = ssn_norm(seed(1):seed(1)+noise2use_points); % take that noise segment
          
          %Speech in Speech-shaped noise:
-         tmpSiSSN = sigs_filt_norm_adj{i}' + noise2use(1+head_points:head_points+length(sigs_filt_norm_adj{i}));
+         SiSSNpart = sigs_filt_norm_adj{i}' + noise2use(1+head_points:head_points+length(sigs_filt_norm_adj{i}));
          ssn_head = noise2use(1:head_points);
          ssn_tail = noise2use((end-tail_points)+1:end);
-         SiSSN{i,1} = [ssn_head.*rampin, tmpSiSSN, ssn_tail.*rampout];
+         SiSSN{i,1} = [ssn_head.*rampin, SiSSNpart, ssn_tail.*rampout];
          
-         %Normalize the combined signal + noise to avoid clipping 
+         %Normalize to [-1 1] range the combined signal + noise to avoid clipping 
          SiSSN{i,1} = normalize(SiSSN{i,1}, 'range',[-1 1]);
-     end
+    end
      
-     %% Plot and save    
+   %% Save audio and summary plots 
+     audio2save= cell(length(SiSSN),1);
      for i = 1:length(SiSSN)
          
-         % Save audio
+         %%% Save audio (speech in speech-shaped noise
+         %--------------------------------------------
          [pathstr, name , ext] = fileparts(wavfiles{i});
          outputfilename = strrep([diroutput,'SiSSN_',name,num2str(dblevel),'db',ext],'\\','\');
          audiowrite(outputfilename, SiSSN{i},srate)
          disp(['...saved ',outputfilename]);
          
-         % Figures
-         %-------------------------
+         %%% Figures of original and SiSSN
+         %---------------------------------------
          SiSSN2plot = SiSSN{i};
          original2plot = audioread(wavfiles{i})';
-         variables = {'original2plot','SiSSN2plot'};
-         
-         %
-         infotext = ['SSN from ', num2str(length(wavfiles)),' files (',num2str(srate),' Hz) concat and filt (',num2str(filt_low),...
+         variables2plot = {'original2plot','SiSSN2plot'};
+         footnote = ['SSN from ', num2str(length(wavfiles)),' files (srate: ',num2str(srate),' Hz) concat and filt (',num2str(filt_low),...
              ' ',num2str(filt_upper),' Hz). Extra noise intro (', num2str(head_length),' s), outro (', num2str(tail_length),' s) with ramps.',...
              ' Noct ',num2str(noctaves),'. NFFT ',num2str(nfft),'. SSN norm-',num2str(dblevel),'db'];
          
+         % Start figure 
          figure ('position', [1 1 800 800],'color','white');
-         annotation('textbox', [0, 0.075, 1, 0], 'string',infotext)
-         % Amplitude x  time
+         annotation('textbox', [0, 0.075, 1, 0], 'string',footnote)
+         
+         % PLOTS: Amplitude x  time
          titles = {'speech (filt)','SiSSN(norm)'};
          for p = 1:2
              subplot(3,2,p);
-             plot(eval(variables{p}))
+             plot(eval(variables2plot{p}))
              title(titles{p});  ylabel('Amplitude (a.u.)');  xlabel('Time (ms)');
          end
-         % Spectral plots
+         % PLOTS: Spectral plots
          titles = {'LTAS speech (filt)','LTAS SiSSN (norm)'};
          for p = 1:2
              subplot(3,2,2+p);
-             iosr.dsp.ltas(eval(variables{p}),srate,'noct',noctaves,'graph',true,'units','none','scaling','max0','win',srate/10);  % requires the IoSR Matlab Toolbox
+             iosr.dsp.ltas(eval(variables2plot{p}),srate,'noct',noctaves,'graph',true,'units','none','scaling','max0','win',srate/10);  % requires the IoSR Matlab Toolbox
              xline(50, '--k'); xline(5000, '--k');
              title(titles{p});
          end
-         % Surf plots
+         % PLOTS: Surf plots
          titles = {'Spectrogram speech(filt)', 'Spectrogram SiSSN(norm)'};
          for p = 1:2
              subplot(3,2,4 + p);
              parameter = [];
              parameter.fsAudio = srate;
              parameter.zeroPad = srate/10;
-             [spec2plot,f,t] = stft(eval(variables{p})',parameter);
+             [spec2plot,f,t] = stft(eval(variables2plot{p})',parameter);
              surf(t,f,abs(spec2plot));
              hold on; set(gcf,'renderer','zbuffer'); shading interp; view(0,90); axis tight; caxis([0 50])
              title(titles{p});
@@ -171,7 +172,7 @@ sigs_filt_norm = cellfun(@(x) x.*RMS_median/rms(x), sigs_filt, 'UniformOutput', 
          
          print(gcf, '-djpeg', strrep(outputfilename,'.wav','.jpg'));
          disp(['...saved figure for ',outputfilename]);
-         %
          close gcf
-     end
+     
+     end %end SiSSN loop   
  end % close noise level loop
