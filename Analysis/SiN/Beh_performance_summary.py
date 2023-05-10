06a38f33a2ce1742d0bfabbd97f923ee48c3404c
gorkafraga <gorkafraga@gmail.com>
2023-05-10 16:00:30 +0200

    Update Beh_performance_summary.py

--- Analysis/SiN/Beh_performance_summary.py
+++ Analysis/SiN/Beh_performance_summary.py
@@ -13,32 +13,37 @@ import os
 import shutil
 import pandas as pd 
 
-rawdir = 'V:/Projects/Spinco/SINEEG/Data/SiN/raw/p001'
-diroutput = 'V:/Projects/Spinco/SINEEG/Outputs/SiN/p001'
+rawdir = 'V:/Projects/Spinco/SINEEG/Data/SiN/raw/pilot/Sibylle'
+diroutput = 'V:/Projects/Spinco/SINEEG/Outputs/SiN/pilot/Sibylle'
 os.makedirs(diroutput, exist_ok=True);
+copyraw = 1;
+
 
 # %% Copy raw data into the analysis folder 
-rawfiles = os.listdir(rawdir)
-# Loop through the files to find the CSV file
-for file in rawfiles:
-    if file.endswith(".csv") and file[-5].isdigit():
-        # If the file is a CSV file, copy it to the output directory
-        filepath = os.path.join(rawdir, file)
-        outputpath = os.path.join(diroutput, file)
-        shutil.copyfile(filepath, outputpath)
+if copyraw == 1:
+    rawfiles = os.listdir(rawdir)
+    # Loop through the files to find the CSV file
+    for file in rawfiles:
+        if file.endswith(".csv") and file[-5].isdigit():
+            # If the file is a CSV file, copy it to the output directory
+            filepath = os.path.join(rawdir, file)
+            outputpath = os.path.join(diroutput, file)
+            
+            shutil.copyfile(filepath, outputpath)
+            print(['saved ' + outputpath])
 
 # %% Read and summary from Analysis folder 
-files = os.listdir(rawdir)
+files = os.listdir(diroutput)
 
 # Loop through the files to find the CSV file
 for file in files:
     if file.endswith(".csv") and file[-5].isdigit():
         filepath = os.path.join(diroutput, file)
  
-        # %% read data frame 
+        # % read data frame 
         df = pd.read_csv(filepath)
-        df = df.iloc[:, :-1] # SEEMS to read an additional blank column [!!!revise if in all files]
-        
+        #df = df.iloc[:, :-1] # SEEMS to read an additional blank column [!!!revise  ]
+        df = df.iloc[:-1, :-1]
         # % Assess accuracy         
         map_call = {'Ad':'call1','Dr':'call2','Kr':'call3','Ti':'call4'}
         map_col = {'ge':'colour1','gr':'colour2','ro':'colour3','we':'colour4'}
@@ -50,10 +55,14 @@ for file in files:
         df['mouseClickOnNumber.clicked_name'] = df['mouseClickOnNumber.clicked_name'].apply(lambda x: x.replace('[\'', '').replace('\']', ''))       
         
         # compare response with presented stimuli
-        sum(df['mouseClickOnCall.clicked_name'] == df.callSign.replace(map_call))
-        sum(df['mouseClickOnColour.clicked_name'] == df.colour.replace(map_col))
-        sum(df['mouseClickOnNumber.clicked_name'] == df.number.replace(map_num))
+        df.insert(0, 'accu_call', (df['mouseClickOnCall.clicked_name'] == df.callSign.replace(map_call)).astype(int))
+        df.insert(0, 'accu_col', (df['mouseClickOnColour.clicked_name'] == df.colour.replace(map_col)).astype(int))
+        df.insert(0, 'accu_num', (df['mouseClickOnNumber.clicked_name'] == df.number.replace(map_num)).astype(int))
 
+# %        
+        ## Summarize accuracy
+        res=  (df.groupby(['noise', 'block', 'levels'])[[ 'accu_call', 'accu_col','accu_num',]].sum())*100/32
+        
 # %% 
 
 
