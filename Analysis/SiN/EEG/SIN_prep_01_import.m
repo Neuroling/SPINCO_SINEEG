57766cba0fe658ec38de02f8920c068eb9fbd6c7
gorkafraga <gorkafraga@gmail.com>
2023-06-08 14:26:20 +0200

    Update SIN_prep_01_import.m

--- Analysis/SiN/EEG/SIN_prep_01_import.m
+++ Analysis/SiN/EEG/SIN_prep_01_import.m
@@ -6,40 +6,41 @@ clear; close all ;
 % Description:
 % - Read raw .bdf files
 % - Create and save eeglab dataset 
-% 
-%% Define paths (Use relative path to 'scripts' Folder to find data)
+%% user inputs
+subjID = 'p002';
+taskID = 'task-rest-pre';
+
+
+%% Paths and files (Use relative path to 'scripts' Folder to find data)
 folders = strsplit(matlab.desktop.editor.getActiveFilename, filesep);
 baseDir = fullfile(folders{1:(find(strcmp(folders, 'Scripts'), 1)-1)});
-dirinput = [baseDir,filesep,fullfile('Data','SiN','raw','p001','eeg')];
-
-%% find raw eeg
+dirinput = [baseDir,filesep,fullfile('Data','SiN','raw',subjID,'eeg',taskID)];
+% find raw eeg
+files  = dir([dirinput,filesep,'*.bdf'])
+%%
 eeglab;
-files  = dir([dirinput,filesep,'*.bdf']);
 for i = 1:length(files)
-   
-   % define inputs and output dir 
-   fullFileInput = [files(i).folder,filesep,files(i).name];
-   chanLocsFile = 'V:\Projects\Spinco\SINEEG\Data\Biosemi_73ch_ThetaPhi.elp'
+   % define current subject input and output dir 
+   fullFileInput = [files(i).folder,filesep,files(i).name];   
    diroutput = strrep(files(i).folder,'raw','preproc'); % Assuming only one 'raw' folder is present in the full path 
    mkdir(diroutput)   
    
-   
-   % EEGLAB ------------------------------------------------------
+   %% EEGLAB ------------------------------------------------------
+   eeglab 
    pop_editoptions( 'option_storedisk', 1); % Process only one dataset at a time   
    
    % import 
-   EEG = pop_biosig(fullFileInput, 'importannot','off');% import function options may vary   
+   EEG = pop_readbdf(fullFileInput); % using thoe older function: %EEG = pop_biosig(fullFileInput, 'importannot','off');
    
    % load channel locations
-  % EEG = pop_chanedit(EEG,'load',chanLocsFile,'besa'); 
+   chanLocsFile = [files(i).folder,filesep,[subjID,'_electrodes.tsv']];
+   EEG = pop_chanedit(EEG,'load',chanLocsFile); 
    
-   % downsample 
+   % downsample    
+   EEG = pop_resample( EEG, 512);
    
-   
-   
-   
-   % save dataset 
-   newSetName = strrep(files(i).name,'.bdf','');
+      % save dataset 
+   newSetName = strrep(files(i).name,'.bdf','_imp');
    EEG  = pop_newset(ALLEEG, EEG, 0,'setname',newSetName,'gui','off','overwrite','on'); 
    EEG = pop_saveset (EEG, newSetName,diroutput);   
    
