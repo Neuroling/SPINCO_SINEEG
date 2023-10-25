0acece1c572dcbc34f594c7a46eebae033bc2d13
samuelmull <108412416+samuelmull@users.noreply.github.com>
2023-10-25 14:20:22 +0200

    Added metadata to epoch
    - still need to make sure the event_ids are better. Need to figure out how to do that.

--- Analysis/SiN/EEG/3_analysis/ANA_01_Read_eeglabEpochs.py
+++ Analysis/SiN/EEG/3_analysis/ANA_01_Read_eeglabEpochs.py
@@ -23,53 +23,65 @@ import MVPA.ANA_01_helper as hp
 # inputs and paths Paths
 taskID = 'task-sin'
 pipeID = 'pipeline-01'
-subjID='s015'
+subjID='s011'
 
 
 thisDir = os.path.dirname(os.path.abspath(__file__))
 subjIDs=[item for item in os.listdir(os.path.join(thisDir[:thisDir.find('Scripts')] + 'Data','SiN','rawdata')) if item[-1].isdigit()]
+
+#%% input directories
 dirinput = os.path.join(thisDir[:thisDir.find('Scripts')] + 'Data','SiN','derivatives', pipeID, taskID + '_preproc_epoched',subjID)
+set_fp = glob(os.path.join(dirinput, "*_epoched_2.set"), recursive=True)[0]
+epo_fp = set_fp[:set_fp.find('_epoched_2.set')]+'-epo.fif'
 
+#%% Read accu.tsv files for the event ids
+events_fp = os.path.join(thisDir[:thisDir.find('Scripts')] + 'Data','SiN','derivatives', pipeID, taskID, subjID)
+events_fp = glob(os.path.join(events_fp, "*accu.tsv"), recursive=True)[0]
+events_tsv = pd.read_csv(events_fp,sep='\t')
 
+#%% making an array of event ids to add as metadata
+event_ids = np.zeros(shape=(1152,3))
+idx = 0
+for i in range(1,len(events_tsv)):
+    if np.isnan(events_tsv['ACCURACY'][i]):
+        continue
+    else:
+        event_ids[idx] = [(events_tsv['SAMPLES'][i]),(events_tsv['VALUE'][i]),(events_tsv['ACCURACY'][i])]
+        idx=idx+1
+        
+event_ids=pd.DataFrame(event_ids,columns=['tf','stim_code','accuracy'])
+event_ids['accuracy'].replace(0,'inc', inplace=True)
+event_ids['accuracy'].replace(1,'cor', inplace=True)
+event_ids['block']=event_ids['stim_code']
+event_ids['block'].replace([111,112,113,114,121,122,123,124,131,132,133,134],'NV',inplace=True)
+event_ids['block'].replace([211,212,213,214,221,222,223,224,231,232,233,234],'NV',inplace=True)
+event_ids['stim']=event_ids['stim_code']
+event_ids['stim'].replace([111,112,113,114,211,212,213,214],'CallSign',inplace=True)
+event_ids['stim'].replace([121,122,123,124,221,222,223,224],'Colour',inplace=True)
+event_ids['stim'].replace([131,132,133,134,231,232,233,234],'Number',inplace=True)
 #%% creating .fif files - takes a lot of ressources, only run if necessary
 # for subjID in subjIDs:
 #     dirinput = os.path.join(thisDir[:thisDir.find('Scripts')] + 'Data','SiN','derivatives', pipeID, taskID + '_preproc_epoched',subjID)
-#     fileinput = glob(os.path.join(dirinput, "*_epoched.set"), recursive=True)[0]
-#     print(fileinput)
-#     hp.eeglabEpo2mneEpo(fileinput)
+#     set_fp = glob(os.path.join(dirinput, "*_epoched.set"), recursive=True)[0]
+#     print(set_fp)
+#     hp.eeglabEpo2mneEpo(set_fp)
 
 
-# #%% Read accu.tsv files for the event ids
-# events_fp = os.path.join(thisDir[:thisDir.find('Scripts')] + 'Data','SiN','derivatives', pipeID, taskID, subjID)
-# events_fp = glob(os.path.join(events_fp, "*accu.tsv"), recursive=True)[0]
-# events_tsv = pd.read_csv(events_fp,sep='\t')
+#%% read epochs from eeglab .set file & save as mne .fif file
+epochs = mne.io.read_epochs_eeglab(set_fp)
+epochs.metadata=event_ids
+epochs.save(epo_fp, overwrite=True, fmt='double')
 
-# #%% making an array of event ids to add to the epochs file
-# event_ids = np.zeros(shape=(1152,3))
-# idx = 0
-# for i in range(1,len(events_tsv)):
-#     if np.isnan(events_tsv['ACCURACY'][i]):
-#         continue
-#     else:
-#         event_ids[idx] = [(events_tsv['SAMPLES'][i]),(events_tsv['VALUE'][i]),(events_tsv['ACCURACY'][i])]
-#         idx=idx+1
-        
-# del events_fp, events_tsv
-# event_idds = [1,0]
+#%% Read epochs from .fif file
+epo = mne.read_epochs(epo_fp)
+epo.plot(events=True)
 
-#%% from set file
-fileinput = glob(os.path.join(dirinput, "*_epoched_2.set"), recursive=True)[0]
-epochs = mne.io.read_epochs_eeglab(fileinput)
+#%% epoch to evoked
+evo = epo.average(by_event_type=True)
 
 
+#%% Create metadata
 
-#%% Read epochs
-#epochs.events= event_ids.astype(int)
-
-epo_fn = fileinput[:fileinput.find('_epoched_2.set')]+'-epo.fif'
-epochs.save(epo_fn, overwrite=True, fmt='double')
-epo = mne.read_epochs(epo_fn)
-#epo.plot()
 
 # %% Read epochs
 
