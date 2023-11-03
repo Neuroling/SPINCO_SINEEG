752621b0294d29ad3c5fde81f935a677c6d6a4c7
gorkafraga <gorkafraga@gmail.com>
2023-11-03 14:39:41 +0100

    set up some values for Sam  to fill in

--- Analysis/SiN/EEG/2_preprocessing/Semi_manual/PrepMa_02_selData_runICA.m
+++ Analysis/SiN/EEG/2_preprocessing/Semi_manual/PrepMa_02_selData_runICA.m
@@ -14,11 +14,10 @@ taskID = 'task-sin';
 dirinput = fullfile(baseDir, 'Data','SiN','derivatives_SM',taskID) ;
 diroutput = dirinput;
 
-
-  
+%% Read table  with badchannels 
 
 %% find data 
-files = dir([dirinput,filesep,'**',filesep,'*',taskID,'*avgRef.set']) 
+files = dir([dirinput,filesep,'**',filesep,'*',taskID,'*.set']) 
 
 %% loop thru files 
 %for f = 1:length(files)
@@ -39,23 +38,24 @@ for f= 1
     %Exclude EOGs 
     EEG = pop_select( EEG, 'rmchannel',{'EXT1','EXT2','EXT3','EXT4'});
 
+    %% EXCLUDE BAD CHANNELS 
+     % Compare subjID in filename and in table with bad channels
+     
+     % find bad channels for this subject 
+     
+     
+     % exclude from eeg    
+    EEG = pop_select( EEG, 'rmchannel',{'here the bad channels '});
+    
+     
+    
     %% 
     % run ICA reducing the dimension by 1 to account for average reference     
-    EEG = pop_runica(EEG, 'icatype','runica','chanind',[1:64]);
-    
-    % Filter 
-    % highpass (aggressive for ICA) 
-    
-
-    %% save before downsampling
-    newsetname = [ursetname, '_filt_avgRef'];
-    pop_saveset (EEG, newsetname,files(f).folder);
-
-    %% Downsample 
-    EEG = pop_resample(EEG, 128);
+    EEG = pop_runica(EEG, 'icatype','runica','chanind',:);
+  
 
     %% Save 
-    newsetname = [ursetname, '_filt_avgRef_ds'];
+    newsetname = [EEG.setname, '_ICA'];
     pop_saveset (EEG, newsetname,files(f).folder);
 
 end
