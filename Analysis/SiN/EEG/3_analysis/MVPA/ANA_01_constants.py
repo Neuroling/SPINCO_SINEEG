64a40618ae16e8210569a08bec5f0acd91144cd7
samuelmull <108412416+samuelmull@users.noreply.github.com>
2023-11-10 14:56:22 +0100

    Epoching: event labels work now, + working on documentation
    To do:
    - better documentation and comments
    - organise files better
    - find a way to update matplotlib on the virtual env

--- Analysis/SiN/EEG/3_analysis/MVPA/ANA_01_constants.py
+++ Analysis/SiN/EEG/3_analysis/MVPA/ANA_01_constants.py
@@ -15,7 +15,45 @@ setFileEnd = '_epoched_2.set'
 thisDir = os.path.dirname(os.path.abspath(__file__))
 subjIDs=[item for item in os.listdir(os.path.join(thisDir[:thisDir.find('Scripts')] + 'Data','SiN','rawdata')) if item[-1].isdigit()]
 
+##### Needed for debugging
 # set_fp = glob(os.path.join(dirinput, str("*"+ setFileEnd)), recursive=True)[0]
 # epo_fp = set_fp[:set_fp.find(setFileEnd)]+'-epo.fif'
 # events_fp = glob(os.path.join(thisDir[:thisDir.find('Scripts')] + 'Data','SiN','derivatives', pipeID, taskID, subjID,"*accu.tsv"), recursive=True)[0]
 # beh_fp = glob(os.path.join(thisDir[:thisDir.find('Scripts')] + 'Data','SiN','rawdata', subjID, taskID, 'beh',"*.csv"), recursive=True)[0]
+
+#########################################################
+
+"""
+These are the event labels:
+    NoiseType / StimulusType / DegradationLevel / Accuracy / Voice
+    
+    X____ NoiseType: NV = 1, SSN = 2
+    _X___ Stimulus Type: Call = 1, Colour = 2, Number = 3
+    __X__ Degradation Level: Lv1 = 1, Lv2 = 2, Lv3 = 3
+    ___X_ Accuracy: Incorrect = 0, Correct = 1
+    ____X Voice: Feminine (Neural2-F) = 1, Masculine (Neural2-D) = 2
+    
+This allows you to filter the epochs using the event labels, i.e. by:
+    epochs.__getitem__('NV') --------> will return all epochs with NV
+    epochs.__getitem__('Lv1/call') --> will return all epochs with Lv1 degradation and CallSign
+"""
+
+event_id = {'NV/call/Lv1/inc/F':11101, 'NV/call/Lv1/inc/M':11102, 'NV/call/Lv1/cor/F':11111, 'NV/call/Lv1/cor/M':11112,
+            'NV/call/Lv2/inc/F':11201, 'NV/call/Lv2/inc/M':11202, 'NV/call/Lv2/cor/F':11211, 'NV/call/Lv2/cor/M':11212,
+            'NV/call/Lv3/inc/F':11301, 'NV/call/Lv3/inc/M':11302, 'NV/call/Lv3/cor/F':11311, 'NV/call/Lv3/cor/M':11312,
+            'NV/col/Lv1/inc/F':12101, 'NV/col/Lv1/inc/M':12102, 'NV/col/Lv1/cor/F':12111, 'NV/col/Lv1/cor/M':12112,
+            'NV/col/Lv2/inc/F':12201, 'NV/col/Lv2/inc/M':12202, 'NV/col/Lv2/cor/F':12211, 'NV/col/Lv2/cor/M':12212,
+            'NV/col/Lv3/inc/F':12301, 'NV/col/Lv3/inc/M':12302, 'NV/col/Lv3/cor/F':12311, 'NV/col/Lv3/cor/M':12312,
+            'NV/num/Lv1/inc/F':13101, 'NV/num/Lv1/inc/M':13102, 'NV/num/Lv1/cor/F':13111, 'NV/num/Lv1/cor/M':13112,
+            'NV/num/Lv2/inc/F':13201, 'NV/num/Lv2/inc/M':13202, 'NV/num/Lv2/cor/F':13211, 'NV/num/Lv2/cor/M':13212,
+            'NV/num/Lv3/inc/F':13301, 'NV/num/Lv3/inc/M':13302, 'NV/num/Lv3/cor/F':13311, 'NV/num/Lv3/cor/M':13312,
+            'SSN/call/Lv1/inc/F':21101, 'SSN/call/Lv1/inc/M':21102, 'SSN/call/Lv1/cor/F':21111, 'SSN/call/Lv1/cor/M':21112,
+            'SSN/call/Lv2/inc/F':21201, 'SSN/call/Lv2/inc/M':21202, 'SSN/call/Lv2/cor/F':21211, 'SSN/call/Lv2/cor/M':21212,
+            'SSN/call/Lv3/inc/F':21301, 'SSN/call/Lv3/inc/M':21302, 'SSN/call/Lv3/cor/F':21311, 'SSN/call/Lv3/cor/M':21312,
+            'SSN/col/Lv1/inc/F':22101, 'SSN/col/Lv1/inc/M':22102, 'SSN/col/Lv1/cor/F':22111, 'SSN/col/Lv1/cor/M':22112,
+            'SSN/col/Lv2/inc/F':22201, 'SSN/col/Lv2/inc/M':22202, 'SSN/col/Lv2/cor/F':22211, 'SSN/col/Lv2/cor/M':22212,
+            'SSN/col/Lv3/inc/F':22301, 'SSN/col/Lv3/inc/M':22302, 'SSN/col/Lv3/cor/F':22311, 'SSN/col/Lv3/cor/M':22312,
+            'SSN/num/Lv1/inc/F':23101, 'SSN/num/Lv1/inc/M':23102, 'SSN/num/Lv1/cor/F':23111, 'SSN/num/Lv1/cor/M':23112,
+            'SSN/num/Lv2/inc/F':23201, 'SSN/num/Lv2/inc/M':23202, 'SSN/num/Lv2/cor/F':23211, 'SSN/num/Lv2/cor/M':23212,
+            'SSN/num/Lv3/inc/F':23301, 'SSN/num/Lv3/inc/M':23302, 'SSN/num/Lv3/cor/F':23311, 'SSN/num/Lv3/cor/M':23312,
+            }
--- Analysis/SiN/EEG/3_analysis/MVPA/ANA_01_helper.py
+++ Analysis/SiN/EEG/3_analysis/MVPA/ANA_01_helper.py
@@ -13,7 +13,21 @@ import pandas as pd
 import numpy as np
 
 class EpochManager:
+    """
+    EpochManager is an object to handle reading & saving epochs and related things like metadata and event_ids.
+    Calling this object requires a SubjID as an input, which will be used to determine file paths.
+    It must therefore be called seperately for each subject.
+    
+    """
     def __init__(self, subjID):
+        """
+        initialising function for EpochManager, used to determine file paths
+
+        Parameters
+        ----------
+        subjID : subject ID
+
+        """
         self.subjID = subjID
         self.thisDir = const.thisDir
         self.dirinput = os.path.join(self.thisDir[:self.thisDir.find('Scripts')] + 'Data','SiN','derivatives', const.pipeID, const.taskID + '_preproc_epoched',subjID)
@@ -21,39 +35,52 @@ class EpochManager:
         self.epo_fp = self.set_fp[:self.set_fp.find(const.setFileEnd)]+'-epo.fif'
         self.events_fp = glob(os.path.join(self.thisDir[:self.thisDir.find('Scripts')] + 'Data','SiN','derivatives', const.pipeID, const.taskID, subjID,"*accu.tsv"), recursive=True)[0]
         self.beh_fp = glob(os.path.join(self.thisDir[:self.thisDir.find('Scripts')] + 'Data','SiN','rawdata', subjID, const.taskID, 'beh',"*.csv"), recursive=True)[0]
+        # self.metadata = 0
     
     def readEpo(self):
+        """
+        reads the epoched .fif file using the filepaths set by EpochManager
+        """
         epo = mne.read_epochs(self.epo_fp)
         return epo
     
-    def set2fif(self,fileinput=None,fileoutput=None,metadata=False):
-        """ takes the epoched .set file from EEGLAB and returns an mne.Epochs object
+    def set2fif(self,fileinput=None,fileoutput=None,addMetadata=False,relabelEvents=False):
+        """ takes the epoched .set file from EEGLAB and saves it as .fif file from MNE
         
         parameters: 
-            fileinput : eeglab epoch file ending in .set (default is set_fp)
-            fileoutput : where the mne file ending in .fif will be saved (default is epo_fp)
-            metadata : if metadata should be constructed and added (default is false)
+            fileinput : eeglab epoch file ending in .set (default is None, which will use the set_fp from EpochManager.__init__)
+            fileoutput : where the mne file ending in .fif will be saved (default is None, which will use the epo_fp from EpochManager.__init__)
+            addMetadata : if metadata should be constructed and added (default is false)
+            relabelEvents: if events should be relabelled (default is false)
+        
+        returns:
+            Nothing. To open the freshly saved epoch, call EpochManager.readEpo()
         """
+        if relabelEvents==True and addMetadata==False:
+            print('warning: metadata will be constructed to relabel events but will not be added')
         if fileinput is None:
             fileinput = self.set_fp
         if fileoutput is None:
             fileoutput = self.epo_fp
         epochs = mne.io.read_epochs_eeglab(fileinput)
-        if metadata==True:
-            epochs = self.AddMetadata(epochs)
+        if addMetadata==True:
+            epochs = self.addMetadata(epochs)
+        if relabelEvents==True:
+            epochs = self.relabelEvents(epochs)
         epochs.save(fileoutput, overwrite=True, fmt='double')
-        epo = mne.read_epochs(fileoutput)
-        return epo
+
     
-    def AddMetadata(self, epochs):
-        """ adds some metadata to the epochs. The metadata is taken from the
+    def constructMetadata(self, epochs):
+        """ constructs some metadata for the epochs. The metadata is taken from the
         accu.tsv file and the behavioural output .csv file
+        
+        This function is called by addMetadata
         
         """
         events_tsv = pd.read_csv(self.events_fp,sep='\t')
         beh_csv = pd.read_csv(self.beh_fp)
         
-        # Adding the info from the tsv to the metadat array
+        ## Adding the info from the tsv to the metadat array
         tmp = len(events_tsv) - events_tsv['ACCURACY'].isnull().sum()
         metadat = np.zeros(shape=(tmp,3))
         idx = 0
@@ -64,25 +91,27 @@ class EpochManager:
                 metadat[idx] = [(events_tsv['SAMPLES'][i]),(events_tsv['VALUE'][i]),(events_tsv['ACCURACY'][i])]
                 idx=idx+1
         
-        # re-labelling the stim_codes to make them more legible        
+        ## re-labelling the stim_codes to make them more legible        
         metadat=pd.DataFrame(metadat,columns=['tf','stim_code','accuracy'])
         metadat['accuracy'].replace(0,'inc', inplace=True)
         metadat['accuracy'].replace(1,'cor', inplace=True)
         metadat['block']=metadat['stim_code']
         metadat['block'].replace([111,112,113,114,121,122,123,124,131,132,133,134],'NV',inplace=True)
-        metadat['block'].replace([211,212,213,214,221,222,223,224,231,232,233,234],'NV',inplace=True)
+        metadat['block'].replace([211,212,213,214,221,222,223,224,231,232,233,234],'SSN',inplace=True)
         metadat['stim']=metadat['stim_code']
         metadat['stim'].replace([111,112,113,114,211,212,213,214],'CallSign',inplace=True)
         metadat['stim'].replace([121,122,123,124,221,222,223,224],'Colour',inplace=True)
         metadat['stim'].replace([131,132,133,134,231,232,233,234],'Number',inplace=True)
         
-        #adding the info from the csv to the metadat array
+        ## reading the info from the csv to the metadat array
         beh_csv = beh_csv[['voice', 'levels', 'callSignCorrect','colourCorrect','numberCorrect']]
         beh_csv=beh_csv.replace(['-11db','0.2p'], 'Lv3')
         beh_csv=beh_csv.replace(['-9db','0.4p'], 'Lv2')
         beh_csv=beh_csv.replace(['-7db','0.6p'], 'Lv1')
         levels_metadat = list()
         voice_metadat = list()
+        
+        ## excluding trials with no response
         for i in range(1,len(beh_csv)):
             if str(beh_csv['levels'][i]).startswith('L'):
                 if beh_csv['callSignCorrect'][i] != "NO_ANSW":
@@ -94,10 +123,63 @@ class EpochManager:
                 if beh_csv['numberCorrect'][i] != "NO_ANSW":
                     levels_metadat.append(beh_csv['levels'][i])
                     voice_metadat.append(beh_csv['voice'][i])
-                    
+        
+        ## adding it to the metadata
         metadat['levels'] = levels_metadat
         metadat['voice'] = voice_metadat
+        self.metadata = metadat
         
-        epochs.metadata = metadat
+    def addMetadata(self,epochs):
+        """
+        adds metadata to the epochs
+        metadata is constructed by calling constructMetadata
+
+        """
+        self.constructMetadata(epochs)
+        epochs.metadata = self.metadata
         return epochs
+    
+    def relabelEvents(self,epochs,metadata=None):
+        """
+        relabels events and event_id using the metadata.
+        event_id dict is taken from constants
+        
+
+        Parameters
+        ----------
+        epochs : epochs.EpochsFIF
+            the epoched data
+        metadata : metadata. Default is None, which will use the metadata from constructMetadata
+
+        Returns
+        -------
+        epochs : epochs.EpochsFIF
+            epoched data with relabelled events and event_id
+        """
+        if metadata != None:
+            mtdat = metadata
+        else:
+            # if self.metadata == 0:
+            #     self.constructMetadata(epochs)
+            # else:
+                mtdat = self.metadata
+        
+        mtdat['block'].replace('NV',1,inplace=True)
+        mtdat['block'].replace('SSN',2,inplace=True)
+        mtdat['stim'].replace('CallSign',1,inplace=True)
+        mtdat['stim'].replace('Colour',2,inplace=True)
+        mtdat['stim'].replace('Number',3,inplace=True)
+        mtdat['levels'].replace('Lv1',1, inplace=True)
+        mtdat['levels'].replace('Lv2',2, inplace=True)
+        mtdat['levels'].replace('Lv3',3, inplace=True)
+        mtdat['accuracy'].replace('inc',0, inplace=True)
+        mtdat['accuracy'].replace('cor',1, inplace=True)
+        mtdat['voice'].replace('Neural2-F',1,inplace=True)
+        mtdat['voice'].replace('Neural2-D',2,inplace=True)
+        
+        for epIdx in range(len(epochs.events)):
+            epochs.events[epIdx][2]=mtdat['block'][epIdx]*10000+ mtdat['stim'][epIdx]*1000+ mtdat['levels'][epIdx]*100+ mtdat['accuracy'][epIdx]*10+ mtdat['voice'][epIdx]
+        epochs.event_id = const.event_id       
+        return epochs
+  
     
