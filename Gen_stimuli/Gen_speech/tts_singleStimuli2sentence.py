542cbe1715bac750d918dce0625c241b5ee213a3
samuelmull <108412416+samuelmull@users.noreply.github.com>
2024-04-30 10:40:05 +0200

    Exp2: combining .wav files
    Commit as backup before I change some stuff.

--- Gen_stimuli/Gen_speech/tts_singleStimuli2sentence.py
+++ Gen_stimuli/Gen_speech/tts_singleStimuli2sentence.py
@@ -6,7 +6,7 @@ Created on Fri Apr 26 12:26:24 2024
 @author: testuser
 """
 import wave
-from itertools import product
+
 import os
 from glob import glob
 
@@ -28,58 +28,75 @@ commonWords = ['Vorsicht', 'gehe sofort zum', 'Feld der Spalte']
 lang_voice_speaker = 'DE_Neural2-F'
 
 # use those to create lists of filepaths
-filename = os.path.join(dirinput, (lang_voice_speaker+'_'+'*'+'.wav'))
-os.path.exists(dirinput)
-audiofiles = glob(filename, recursive= True)
-# TODO
+# Yes this could be done easier, more efficiently, and with less possibility for human error. 
+# (i.e. with itertools.product)
+# But I have not had lunch, it is 16.30 on a Friday, and this works, so we're going with this :)
+combinations = []
+combination_name = []
+for i in callSign:
 
-def stitch_wav_files(input_files, output_file):
+    for ii in colour:
+
+        for iii in number: 
+            thisCallSign = i[:4]
+            thisColour   = ii[:4]
+            thisNumber   = iii[:4]
+            
+            combination_name.append(thisCallSign + '-' + thisColour + '-' + thisNumber)
+            files = []
+            files.append(glob(os.path.join(dirinput, (lang_voice_speaker+'_'+commonWords[0][:4]+'.wav')))[0])
+            files.append(glob(os.path.join(dirinput, (lang_voice_speaker+'_'+thisCallSign+'.wav')))[0])
+            
+            files.append(glob(os.path.join(dirinput, (lang_voice_speaker+'_'+commonWords[1][:4]+'.wav')))[0])
+            files.append(glob(os.path.join(dirinput, (lang_voice_speaker+'_'+thisColour+'.wav')))[0])
+            
+            files.append(glob(os.path.join(dirinput, (lang_voice_speaker+'_'+commonWords[2][:4]+'.wav')))[0])
+            files.append(glob(os.path.join(dirinput, (lang_voice_speaker+'_'+thisNumber+'.wav')))[0])
+            combinations.append(files)
+
+   
+
+
+for idx, combination in enumerate(combinations):
+    # Create a unique filename for each combination
+    output_file = os.path.join(diroutput, lang_voice_speaker + '_' +  combination_name[idx] + '.wav')
+    
+    # Stitch the files in the current combination together
     # Open the output file in write mode
-    output_wav = wave.open(output_file, 'wb')
+    with wave.open('output_file', mode = 'wb') as output_wav:
+        print('1')
+        # Initialize variables for the output file parameters
+        num_channels = 0
+        sample_width = 0
+        frame_rate = 0
+        num_frames = 0
+        
+    
+        for input_file in combination:
+            # Open each input file
+            
+            with wave.open(input_file, 'rb') as input_wav:
+                # If this is the first file, set the output parameters
+                if num_channels == 0:
+                    num_channels = input_wav.getnchannels()
+                    sample_width = input_wav.getsampwidth()
+                    frame_rate = input_wav.getframerate()
+    
+                # Read frames from input file and write to output file
+                frames = input_wav.readframes(input_wav.getnframes())
+                output_wav.writeframes(frames)
+                num_frames += input_wav.getnframes()
+        print('2')
+        # Update the output file parameters
+        output_wav.setnchannels(num_channels)
+        output_wav.setsampwidth(sample_width)
+        output_wav.setframerate(frame_rate)
+        output_wav.setnframes(num_frames)
+        print('3')
+        # Close the output file
+        output_wav.close()
 
-    # Initialize variables for the output file parameters
-    num_channels = 0
-    sample_width = 0
-    frame_rate = 0
-    num_frames = 0
 
-    for input_file in input_files:
-        # Open each input file
-        with wave.open(input_file, 'rb') as input_wav:
-            # If this is the first file, set the output parameters
-            if num_channels == 0:
-                num_channels = input_wav.getnchannels()
-                sample_width = input_wav.getsampwidth()
-                frame_rate = input_wav.getframerate()
-
-            # Read frames from input file and write to output file
-            frames = input_wav.readframes(input_wav.getnframes())
-            output_wav.writeframes(frames)
-            num_frames += input_wav.getnframes()
-
-    # Update the output file parameters
-    output_wav.setnchannels(num_channels)
-    output_wav.setsampwidth(sample_width)
-    output_wav.setframerate(frame_rate)
-    output_wav.setnframes(num_frames)
-
-    # Close the output file
-    output_wav.close()
-
-def combine_wav_combinations(files_list1, files_list2, files_list3, output_dir):
-    # Generate all combinations of files from the three lists
-    combinations = product(files_list1, files_list2, files_list3)
-
-    for idx, combination in enumerate(combinations, start=1):
-        # Create a unique filename for each combination
-        output_file = f"{output_dir}/combined_{idx}.wav"
-        # Stitch the files in the current combination together
-        stitch_wav_files(combination, output_file)
-        print(f"Combination {idx} created: {output_file}")
-
-# # Example usage
-# files_list1 = ["file1a.wav", "file1b.wav"]
-# files_list2 = ["file2a.wav", "file2b.wav"]
-# files_list3 = ["file3a.wav", "file3b.wav"]
-# output_dir = "output_folder"
-# combine_wav_combinations(files_list1, files_list2, files_list3, output_dir)
+    print(f"Combination {idx} created: {output_file}")            
+            
+     
